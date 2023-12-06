cacb2dc69fb5f2e2ecace0a1601e4f489eb407d2
Eveghu <eveguther546@gmail.com>
2023-12-06 07:59:46 -0600

    c2

--- resources/views/editproduct.blade.php
+++ resources/views/editproduct.blade.php
@@ -79,20 +79,19 @@
              </div>
             </div>     
             <label for="size">SELECCIONA EL TIPO DE TALLA:</label>
-            <select id="sizeType" name="sizeType" onchange="mostrarOpciones()">
-                <option value="Numerica" {{ $product->sizeType == 'Numerica' ? 'selected' : '' }}>Numérica</option>
-                <option value="Tamaño" {{ $product->sizeType == 'Tamaño' ? 'selected' : '' }}>Tamaño</option>
+            <select id="size" name="size[]" onchange="mostrarOpciones()">
+                <option value="numerica">Numérica</option>
+                <option value="tamaño">Tamaño</option>
             </select>
-            
-            <!-- Opciones de tamaño -->
-            <div id="opcionesTamaño" style="{{ $product->sizeType == 'Tamaño' ? '' : 'display:none;' }}">
-                <div class="row">
-                    <div class="col-2">
-                        <div class="form-check">
-                            <input type="checkbox" name="size[]" class="form-check-input" value="CH" id="size" {{ in_array('CH', $product->size) ? 'checked' : '' }}>
-                            <label class="form-check-label" for="size">CH</label>
-                        </div>
+        <!-- Opciones de tamaño -->
+        <div id="opcionesTamaño" style="display:none;">
+            <div class="row">
+                <div class="col-2">
+                    <div class="form-check">
+                        <input type="checkbox" name="size[]" class="form-check-input" value="CH" id="size">
+                        <label class="form-check-label" for="size">CH</label>
                     </div>
+                </div>
                 <div class="col-2">
                     <div class="form-check">
                         <input type="checkbox" name="size[]" class="form-check-input" value="M" id="size">
